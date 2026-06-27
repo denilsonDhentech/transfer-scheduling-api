3009b3b61400b329e9ff48c3d0509a382de80367
denilsonDhentech <denilson_contato@outlook.com>
2026-06-27 13:53:20 -0300

    feat: add transfer repository extending JpaRepository

---
+++ src/main/java/org/dhentech/infrastructure/repository/TransferRepository.java
@@ -0,0 +1,9 @@
+package org.dhentech.infrastructure.repository;
+
+import org.dhentech.infrastructure.entity.TransferEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransferRepository extends JpaRepository<TransferEntity, Long> {
+}
